819be13f3b1d90597093acfd88373befa6ffa0c0
unknown <ivlin112@gmail.com>
2015-08-09 16:15:08 -0400

    Tried fb plugin still

--- platforms/android/assets/www/js/main.js
+++ platforms/android/assets/www/js/main.js
@@ -1,72 +1,89 @@
 //Parse related keys
-var PARSE_APP = "uRElO79J6tFnbwmC2wAnSTOYhQwfjl7fyUCmPEe2";
-var PARSE_JS = "i5kuiIlJoyLi0RyBwMdK0feNgLyE8OqQcGJomru6";
+var PARSE_APP = "bFpMdQLKzOXnYH7r9wdRRME4JmsZ4oxSae2YrH84";
+var PARSE_JS = "T5dQgHMRBck7xs3Dws2tmhJylLabXaOzebAfVTsg";
+var viewframes = [document.getElementById("view-signin"), document.getElementById("view-signup"), document.getElementById("view-trending")];
 
 $(document).ready(function() {
-	Parse.initialize(PARSE_APP, PARSE_JS);
-	User = Parse.Object.extend("User");
-
-	function get() {
-		var query = new Parse.Query(User);
-
-		query.find({
-			success:function(results) {
-				console.dir(results);
-				var s = "";
-				for(var i=0, len=results.length; i<len; i++) {
-					var note = results[i];
-					s += "<p>";
-					s += "<b>"+note.get("title")+"</b><br/>";
-					s += "<b>Written "+note.createdAt + "<br/>";
-					s += note.get("body");
-					s += "</p>";
-				}
-				$("#notes").html(s);
-			},
-			error:function(error) {
-				alert("Error when getting notes!");
-			}
-		});
-	}
-
-
-	$("#test").on("touchend click", function(e) {
-		e.preventDefault();
-
-		//Grab the note details, no real validation for now
-		//var email = $("#noteTitle").val();
-		//var  = $("#noteBody").val();
-
-		var note = new User();
-		note.save({username:'test', password:'test'}, {
-			success:function(object) {
-				console.log("Saved the object!");
-				//$("#noteTitle").val("");
-				//$("#noteBody").val("");
-				//getNotes();
-			}, 
-			error:function(object,error) {
-				console.dir(error);
-			alert("Sorry, I couldn't save it.");			}
-		});
-	});
-	//call getNotes immediately
-	//getNotes();
-	
+	onDeviceReady();
 });
+
+function onDeviceReady(){
+//	Parse.initialize(PARSE_APP, PARSE_JS);
+initEventList();
+}
 
 //setup trending page
 
 function initEventList(){
+	Parse.initialize(PARSE_APP, PARSE_JS);
 	//var eventListDisplay = document.createElement("div");
 	//eventListDisplay.setAttribute("id", "eventListDisplay");
 	//document.getElementById("display").appendChild(eventListDisplay);
 	//var eventList = new eventList(eventListDisplay);
-	var a = new eventList();
-	a.addEvent(new eventItem("Shakespeare in the Park", "JOE", "FREE", "CENTRAL PARK",
-		"asdasoidnasoidnaow asadiofnwa efitjl flzksefawl ds l lk"));
+	/*
+
+	Event = Parse.Object.extend("Event");
+	EventList = Parse.Collection.extend(
+	{
+		model: Event
+	});
+	var eventList = new EventList();
+	eventList.fetch(
+		{success:function(events){
+			console.log(events);
+		},
+		error:function(error){
+			console.dir(error);
+		}
+	});
+*/
+Event = Parse.Object.extend("Event");
+EventList = Parse.Collection.extend({
+	model: Event
+});
+
+var eventList = new EventList();
+
+eventList.fetch(
+	{success:function(eventList){ //calls twice
+		//console.log("A");
+			//console.log(eventList);
+			var eventListView = new EventListView({ collection: eventList });
+			//console.log(eventListView.el);
+			eventListView.render();
+			//console.log(eventListView.el);
+			//document.getElementById("event-list-display").replaceChild(eventListView.el);
+			$('#event-list-display').html(eventListView.el);  
+
+		},
+		error:function(error){
+			console.dir(error);
+		}
+	});
+
+var EventListView = Parse.View.extend({
+	template:Handlebars.compile(document.getElementById("event-list").innerHTML),
+	render:function(){
+		var collection = {event: this.collection.toJSON()};
+
+		collection.event = sortByKey(collection.event, "title", true);
+		
+		this.$el.html(this.template(collection));
+	}
+});
+
+
+//http://stackoverflow.com/questions/8175093/simple-function-to-sort-an-array-of-objects by David Brainer-Banker
+function sortByKey(array, key, ascending) {
+	return array.sort(function(a, b) {
+		var x = a[key]; var y = b[key];
+		var diff = ((x < y) ? -1 : ((x > y) ? 1 : 0));
+		return ascending ? diff : -1 * diff;
+	});	
 }
 
+}
+/*
 function eventList(){
 	this.eventListDisplay = document.getElementById("eventListDisplay");
 	this.listLength = 0;
@@ -89,8 +106,9 @@ function eventItem(title, organizer, cost, location, description){
 	this.eventLocation = location;
 	this.eventDescription = description;
 }
+*/
 
-//for setting up the lin
+//for setting up the login
 
 function buildLogin(){
 	setupLogin(); 
@@ -99,49 +117,92 @@ function buildLogin(){
 
 function setupLogin(){
 	var temp;
-	
 	temp = document.getElementById("signup-button");
 	if (temp !== null){
-		temp.addEventListener("click", function(e) {
-//			$("#signup-button").on("click", function(e){
-	//		e.preventDefault();
-			var newAccount = new User();
-			newAccount.save({username:'test', password:'test'}, {
-				success:function(object){
-					console.log("saved");
-					location.href = "index.html";
-				}
-			}, {
-				failure:function(object){
-					console.dir(error);
-					alert("object not saved");
-				}
-			});
+		temp.addEventListener("click", function(e){
+			var formName = document.getElementById("form-username").value;
+			var formPass = document.getElementById("form-password").value;
+			var formConfirmPass = document.getElementById("form-confirm-password").value;
+			var formEmail = document.getElementById("form-email").value;
+			if (formName !== "" && formEmail !== "" && formPass !== "" && formConfirmPass === formPass){
+				e.preventDefault();
+				Parse.User.signUp(formName, formPass, {},{
+					success:function(result){
+						console.log("success");
+						document.getElementById("signup-status").innerHTML = "Registration successful";
+					},
+					error:function(error){
+						console.dir(error);
+						document.getElementById("signup-status").innerHTML = "Username already taken<br>Try again";
+					}
+				});
+			}else{
+				document.getElementById("signup-status").innerHTML = "Form incorrectly filled";
+			}
 		});
-	}
+	} 
 	temp = document.getElementById("signin-button");
 	if (temp !== null){
+		temp.addEventListener("click", function(e){
+			var formName = document.getElementById("form-username").value;
+			var formPass = document.getElementById("form-password").value;
+			if (formName !== "" && formPass !== ""){
+				e.preventDefault();
+				Parse.User.logIn(formName, formPass, {
+					success:function(result){
+						location.href = "trending.html";
+					},
+					error:function(error){
+						document.getElementById("signin-status").innerHTML = "Failed to sign in";
+					}
+				});
+			}
+		});
 	}
 }
 
 function setupLinks(){
-	var temp;
-	temp = document.getElementsByClassName("goto-trending");
-	for (var i = 0; i < temp.length; i++){
-		temp[i].addEventListener("click", function(){
-			location.href = "trending.html";
-		});
-	};
-	temp = document.getElementsByClassName("goto-signup");
-	for (var i = 0; i < temp.length; i++){
-		temp[i].addEventListener("click", function(){
-			location.href = "signup.html";
-		});
-	}
-	temp = document.getElementsByClassName("goto-signin")
-	for (var i = 0; i < temp.length; i++){
-		temp[i].addEventListener("click", function(){
-			location.href = "index.html";
-		});
+	var temp = document.getElementsByTagName("button");
+	for (var i = 0; i  < temp.length; i++){
+		switch (temp[i].getAttribute("class")){
+			case "goto-trending":
+			temp[i].addEventListener("click", function(){
+				for (var x = 0; x < viewframes.length; x++){
+					viewframes[x].style.display = "none";
+				}
+				document.getElementById("view-trending").style.display = "inline";
+			});
+			break;
+			case "goto-signup":
+			temp[i].addEventListener("click", function(){
+				for (var x = 0; x < viewframes.length; x++){
+					viewframes[x].style.display = "none";
+				}
+				document.getElementById("view-signup").style.display = "inline";
+			});
+			break;
+			case "goto-signin":
+			temp[i].addEventListener("click", function(){
+				for (var x = 0; x < viewframes.length; x++){
+					viewframes[x].style.display = "none";
+				}
+				document.getElementById("view-signin").style.display = "inline";
+			});
+			break;
+			default:
+			break;
+		}
 	}
 }
+
+/* Facebook login */
+var fbLoginSuccess = function (userData) {
+	alert("UserInfo: " + JSON.stringify(userData));
+};
+
+$('#signin-button').click(function(e) {
+	facebookConnectPlugin.login(["public_profile", "email", "user_friends"],
+		fbLoginSuccess,
+		function (error) { alert("" + error);}
+		);
+});
