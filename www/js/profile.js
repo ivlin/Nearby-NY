4d022f46f0e46b79d8df26c7b408a7e59ebe757e
Alexandra Grant <alexandra@digg.com>
2015-08-19 13:59:43 -0400

    new upload profile picture in the profile settings (this image will need to be saved somewhere if we want it to exist permanently)

--- www/js/profile.js
+++ www/js/profile.js
@@ -1,81 +1,107 @@
 var profile = {
 
- curUser:null, 
+    curUser: null,
 
- initialize: function(){
-  this.setupProfilePage();
-  this.setupHandlers();
-},
+    initialize: function() {
+        this.setupProfilePage();
+        this.setupHandlers();
+    },
 
-setupProfilePage: function(){
-  this.curUser = Parse.User.current().toJSON();
-  this.updateInfo();
-  this.updatePreferenceForm();
-},
+    setupProfilePage: function() {
+        this.curUser = Parse.User.current().toJSON();
+        this.updateInfo();
+        this.updatePreferenceForm();
+    },
 
-setupHandlers: function(){
-  $("#edit-bio").click(function(){
-    profile.drawForm();
-   // document.getElementById("set-bio-info").style.display = "inline";
-   // document.getElementById("get-bio-info").style.display = "none";
-   $("#set-bio-info").css("display", "inline");
-   $("#get-bio-info").css("display", "none");
- });
+    setupHandlers: function() {
+        $("#edit-bio").click(function() {
+            profile.drawForm();
+            // document.getElementById("set-bio-info").style.display = "inline";
+            // document.getElementById("get-bio-info").style.display = "none";
+            $("#set-bio-info").css("display", "inline");
+            $("#get-bio-info").css("display", "none");
+        });
 
-  $("#save-bio").click(function(e){
-    e.preventDefault();
-    Parse.User.current().set("name", $("#set-name").val());
-    Parse.User.current().set("biography", $("#set-bio").val());
-    Parse.User.current().save();
+        $("#save-bio").click(function(e) {
+            e.preventDefault();
+            Parse.User.current().set("name", $("#set-name").val());
+            Parse.User.current().set("biography", $("#set-bio").val());
+            Parse.User.current().save();
 
-    profile.setupProfilePage();
+            profile.setupProfilePage();
 
-    $("#set-bio-info").css("display","none");
-    $("#get-bio-info").css("display","inline");
-  });
+            $("#set-bio-info").css("display", "none");
+            $("#get-bio-info").css("display", "inline");
+        });
 
-  $("#update-event-preferences").click(function(){
-    profile.updateUserPreferences();
-  });
-},
+        $("#set-profile-pic").change(function() {
+            $("#upload-profile-pic").removeClass("disabled");
+        });
 
-updateInfo: function(){
-  $("#get-username").html(profile.curUser.username);
-  $("#get-name").html(profile.curUser.name);
-  $("#get-email").html(profile.curUser.password);
-  $("#get-bio").html(profile.curUser.biography);
-},
+        $("#upload-profile-pic").click(function(e) {
+            imageFile = document.getElementById("set-profile-pic").files[0];
+            preview = $("#profile-preview img");
 
-drawForm: function(){
-  $("#set-username").attr("value", profile.curUser.username);
-  $("#set-name").attr("value", profile.curUser.name);
-  $("#set-email").attr("value", profile.curUser.email);
-  $("#set-bio").html(profile.curUser.biography);
+            var reader = new FileReader();
 
-  var labels = document.getElementsByTagName("label");
-  for (var i = 0; i < labels.length; i++){
-    labels[i].setAttribute("class","active");
-  }
+            reader.onloadend = function() {
+                preview.attr("src", reader.result);
+                $("#profile-preview").show();
+            };
 
-},
+            if (imageFile) {
+                reader.readAsDataURL(imageFile); //reads the data as a URL
+            } else {
+                preview.attr("src", "");
+            }
+        });
 
-updatePreferenceForm: function() {
-  var userTags = profile.curUser.tags;
-  for (var i = 0; i < userTags.length; i++){
-    document.getElementById(userTags[i]).checked = true;
-  }
-},
+        $("#update-event-preferences").click(function() {
+            profile.updateUserPreferences();
+        });
+    },
 
-updateUserPreferences: function() {
-  var tagBoxes = $(".tag-selector");
-  var tagArray = [];
-  for (var i = 0; i < tagBoxes.length; i++){
-    if (tagBoxes[i].checked){
-      tagArray[tagArray.length] = tagBoxes[i].id;
+    updateInfo: function() {
+        $("#get-username").html(profile.curUser.username);
+        $("#get-name").html(profile.curUser.name);
+        $("#get-email").html(profile.curUser.password);
+        $("#get-bio").html(profile.curUser.biography);
+    },
+
+    drawForm: function() {
+        $("#set-username").attr("value", profile.curUser.username);
+        $("#set-name").attr("value", profile.curUser.name);
+        $("#set-email").attr("value", profile.curUser.email);
+        $("#set-bio").html(profile.curUser.biography);
+
+        var labels = document.getElementsByTagName("label");
+        for (var i = 0; i < labels.length; i++) {
+            labels[i].setAttribute("class", "active");
+        }
+
+    },
+
+    updatePreferenceForm: function() {
+        var userTags = profile.curUser.tags;
+        if (!userTags)
+            console.log("No tags.");
+        return;
+
+        for (var i = 0; i < userTags.length; i++) {
+            document.getElementById(userTags[i]).checked = true;
+        }
+    },
+
+    updateUserPreferences: function() {
+        var tagBoxes = $(".tag-selector");
+        var tagArray = [];
+        for (var i = 0; i < tagBoxes.length; i++) {
+            if (tagBoxes[i].checked) {
+                tagArray[tagArray.length] = tagBoxes[i].id;
+            }
+        }
+        Parse.User.current().set("tags", tagArray);
+        Parse.User.current().save();
     }
-  }
-  Parse.User.current().set("tags", tagArray);
-  Parse.User.current().save();
-}
 
 };
