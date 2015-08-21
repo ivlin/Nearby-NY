a506ab4aac06d5bfe5a2a9faece1551645e24872
ChristianJGarrido <chrizgarrido@gmail.com>
2015-08-21 13:39:24 -0400

    Added changes so you can run an android build

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -6,10 +6,10 @@ module.exports = [
         "runs": true
     },
     {
-        "file": "plugins/plugin.google.maps/www/googlemaps-cdv-plugin.js",
-        "id": "plugin.google.maps.phonegap-googlemaps-plugin",
+        "file": "plugins/plugin.http.request/www/http-request.js",
+        "id": "plugin.http.request.phonegap-http-requst",
         "clobbers": [
-            "plugin.google.maps"
+            "cordova.plugins.http-request"
         ]
     },
     {
@@ -18,23 +18,15 @@ module.exports = [
         "clobbers": [
             "facebookConnectPlugin"
         ]
-    },
-    {
-        "file": "plugins/plugin.http.request/www/http-request.js",
-        "id": "plugin.http.request.phonegap-http-requst",
-        "clobbers": [
-            "cordova.plugins.http-request"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "com.google.playservices": "23.0.0",
     "cordova-plugin-whitelist": "1.0.0",
-    "plugin.google.maps": "1.2.5",
-    "com.phonegap.plugins.facebookconnect": "0.11.0",
     "plugin.http.request": "1.0.4",
-    "com.google.playservices": "23.0.0"
+    "com.phonegap.plugins.facebookconnect": "0.11.0"
 }
 // BOTTOM OF METADATA
 });
--- platforms/android/assets/www/js/friends.js
+++ platforms/android/assets/www/js/friends.js
@@ -1,7 +1,7 @@
 var friends = {
 
-    friendList:null,
-    friendListView:null,
+    friendList: null,
+    friendListView: null,
 
     buildList: function() {
         FriendList = Parse.Collection.extend({
@@ -9,30 +9,38 @@ var friends = {
         });
 
         FriendListView = Parse.View.extend({
-            data:null,
-            template:Handlebars.compile(document.getElementById("friend-list-tpl").innerHTML),
-            render:function() {
-                this.data = {friend: this.collection};
+            data: null,
+            template: Handlebars.compile(document.getElementById("friend-list-tpl").innerHTML),
+            render: function() {
+                this.data = {
+                    friend: this.collection
+                };
                 this.el.innerHTML = this.template(this.data);
             }
         });
 
         var friendIds = Parse.User.current().get("friends");
         var query = new Parse.Query(Parse.User);
-        query.containedIn("objectId",friendIds);
-        query.find({success:function(result){
-            for (var i = 0; i < result.length; i++){
-                result[i] = result[i].toJSON();
-            }
-            friends.friendListView = new FriendListView({collection:result});
-            friends.friendListView.render();
-            $("#friend-list-display").append(friends.friendListView.el);
+        query.containedIn("objectId", friendIds);
+        query.find({
+            success: function(result) {
+                for (var i = 0; i < result.length; i++) {
+                    result[i] = result[i].toJSON();
+                    console.log(result[i]);
+                }
+                friends.friendListView = new FriendListView({
+                    collection: result
+                });
+                friends.friendListView.render();
+                $("#friend-list-display").append(friends.friendListView.el);
 
-        },error:function(e){
-            console.dir(e);
-        }});
+            },
+            error: function(e) {
+                console.dir(e);
+            }
+        });
 
 
     },
 
-};  
+};
